4fa958e50b9f19cc427d5fec0532dbf51d3dab0c
engin elkurt <elkurt_engin@hotmail.com>
2024-10-25 17:04:58 +0300

    Ders 8 metotlar eklendi

---
+++ 08_Methods/Program.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08_Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Void(Geriye Deger Dondurmeyen) Metotlar
+
+            //Metotlar mor renk kup ıle belirtilir.
+            //void CustomerList()
+            //{
+            //    Console.WriteLine("Ali Yildiz");
+            //    Console.WriteLine("Ahmet Yildiz");
+            //    Console.WriteLine("Mehmet Yildiz");
+            //    Console.WriteLine("Sinan Dildiz");
+            //}
+
+            //CustomerList();
+            //CustomerList();
+            //CustomerList();
+            //CustomerList();
+
+
+            //void Sum()
+            //{
+            //    int x = 1;
+            //    int y=2;
+            //    int z = x+y;
+            //    Console.WriteLine(z);
+
+            //}
+            //Sum();
+
+
+
+            #endregion
+
+            #region Geriye Deger Dondurmeyen String Parametreli Metotlar
+
+            //void WriteMethod(string customerName)
+            //{
+            //    Console.WriteLine(customerName);
+            //}
+            //WriteMethod("Mehmet Yildirim");
+
+            //void CustomerCard(string name,string surName)
+            //{
+            //    Console.WriteLine("Musteri: " + name + " " + surName);
+            //}
+            //CustomerCard("Mehmet","Yildiz");
+            //CustomerCard("Ayse", "Yildiz");
+            #endregion
+
+
+            #region Geriye Deger Dondurmeyen Int Parametreli Metotlar
+
+            //void Sum (int number1 , int number2, int number3)
+            //{
+            //    int result=number1 + number2 + number3 ;
+            //    Console.WriteLine(result);
+            //}
+            //Sum(4, 5, 6);
+            #endregion
+
+
+
+            #region Geriye Deger Donduren Metotlar
+
+            //string CustomerName ()
+            //{
+            //    return "Buse Yildiz";
+            //}
+            //CustomerName();
+
+            //string StundentCard()
+
+            //{
+            //    string name = "Ali";
+            //    string surname = "Yildiz";
+            //    return name + " " + surname;    
+            //}
+
+            //Console.WriteLine(StundentCard());
+
+
+
+
+            #endregion
+
+
+
+            #region Deger Donduren String Parametreli Metotlar
+
+
+
+            //string CountryCard (string countryName,string capital , string flagColor)
+            //{
+            //    string cardInfo="Ulke: "+ countryName+ "-Baskent: " + capital + "Bayrak Renk: " +flagColor;
+            //    return cardInfo;
+            //}
+            //string x,y,z;
+            //Console.Write("Ulke adini giriniz: ");
+            //x=Console.ReadLine();
+            //Console.Write("Baskenti giriniz: ");
+            //y=Console.ReadLine();
+            //Console.Write("Ulke bayrak rengi giriniz: ");
+            //z=Console.ReadLine();   
+
+            //Console.WriteLine(CountryCard(x,y,z));
+            //Console.WriteLine((CountryCard("Türkiye", "Ankara", "Kırmızı-Beyaz")));
+
+            #endregion
+
+            #region Geriye Deger Donduren Int Parametreli Metotlar
+
+
+            //int Sum(int number1,int number2)
+            //{
+            //    int result = number1 + number2;
+            //    return result;
+
+
+            //}
+            //Console.WriteLine(Sum(45, 99));
+            //Console.WriteLine(Sum(25, 39));
+            //Console.WriteLine(Sum(42, 49));
+            //Console.WriteLine(Sum(15, 39));
+            //Console.WriteLine(Sum(55, 69));
+
+
+
+
+
+
+
+            //string ExamResult(string student, int exam1, int exam2, int exam3)
+            //{
+            //    int result = (exam1 + exam2 + exam3) / 3;
+            //    if (result >= 50)
+            //    {
+            //        return student + " isimli Ogrenci sinavi gecti" + "Ortlama: " + result;
+            //    }
+            //    else
+            //    {
+            //        return student + "Ogrenci basarisiz oldu" + "Ortalama: " + result;
+            //    }
+            //    Console.WriteLine(ExamResult("Ali", 25, 35, 66));
+            //    Console.WriteLine(ExamResult("Ayse", 35, 85, 66));
+            //    Console.WriteLine(ExamResult("Ali", 65, 95, 86));
+            //    Console.WriteLine(ExamResult("Ali", 25, 45, 76));
+
+                #endregion
+            
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+    }
+}
